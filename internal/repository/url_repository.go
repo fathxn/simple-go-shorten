e294b05c3bc70e34fdd8f3b88314e9cf70439fd7
fathxn <fathan0x1@gmail.com>
2024-09-24 22:07:22 +0700

    feat(url): added url repo and usecase methods

--- internal/repository/url_repository.go
+++ internal/repository/url_repository.go
@@ -1,1 +1,25 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"simple-go-shorten/internal/domain"
+)
+
+type urlRepository struct {
+	db *sql.DB
+}
+
+func NewURLRepository(db *sql.DB) domain.URLRepository {
+	return &urlRepository{db: db}
+}
+
+// Create implements domain.URLRepository.
+func (r *urlRepository) Create(ctx context.Context, longURL string) error {
+	panic("unimplemented")
+}
+
+// FindByShortURL implements domain.URLRepository.
+func (r *urlRepository) FindByShortURL(ctx context.Context, shortURL string) error {
+	panic("unimplemented")
+}
--- internal/usecase/url_usecase.go
+++ internal/usecase/url_usecase.go
@@ -1,1 +1,11 @@
 package usecase
+
+import "simple-go-shorten/internal/domain"
+
+type urlUsecase struct {
+	URLRepository domain.URLRepository
+}
+
+func NewURLUsecase(urlRepository domain.URLRepository) domain.URLUsecase {
+	return &urlUsecase{URLRepository: urlRepository}
+}
